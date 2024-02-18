4a026f2cdbc08a74e285d2cc71135987497b5cc8
koplec <koplec>
2024-02-18 21:42:51 +0900

    コメント追記

--- abstract-factory/yuki/factory/Tray.java
+++ abstract-factory/yuki/factory/Tray.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 // 抽象的な部品の一つ
 public abstract class Tray extends Item{
+    // この中にItemを入れる
+    // protectedにしたのは、サブクラスからもアクセスできるようにするため
+    // このメリットは、サブクラスからもアクセスできるため、サブクラスでの実装が楽になる
+    // このデメリットは、サブクラスからもアクセスされてしまうので、カプセル化ができない
     protected List<Item> tray = new ArrayList<>();
     public Tray(String caption) {
         super(caption);
