f0e50dbb7252bbf443ce89e0044c748e87681401
koplec <koplec>
2024-02-20 07:36:15 +0900

    reader.mark, resetメソッドについてコメント追加

--- bridge/yuki/FileDisplayImpl.java
+++ bridge/yuki/FileDisplayImpl.java
@@ -18,6 +18,8 @@ public class FileDisplayImpl extends DisplayImpl{
         try{
             reader = new BufferedReader(new FileReader(filename));
             //markメソッドでマーク位置を設定
+            //reader.mark(MAX_READHEAD_LIMIT)で、ファイルの先頭位置にマークを設定します。
+            //これにより、`rawPrint()` メソッドが複数回呼び出されても、ファイルの内容を最初から再度読み取ることができます。 
             reader.mark(MAX_READHEAD_LIMIT);
         }catch(IOException e){
             e.printStackTrace();
@@ -28,6 +30,7 @@ public class FileDisplayImpl extends DisplayImpl{
     public void rawPrint() {
         try{
             String line;
+            //reader.reset() を呼び出して、ファイルの読み取り位置をマークされた先頭位置に戻します
             reader.reset();
             while((line = reader.readLine()) != null){
                 System.out.println("> " + line);
