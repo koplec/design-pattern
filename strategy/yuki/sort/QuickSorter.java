801d9dfc1a795d843f074c7ad84e174aa2d231a0
koplec <koplec>
2024-02-26 22:59:56 +0900

    コメント追記

--- strategy/yuki/sort/QuickSorter.java
+++ strategy/yuki/sort/QuickSorter.java
@@ -18,13 +18,17 @@ public class QuickSorter<T> implements Sorter<T>{
         Comparable<T> x = data[high];
         int i = low-1;
         for(int j=low; j<high; j++){
-            if(data[j].compareTo((T)x) <= 0){
+            //highの値を基準にして、それよりも低いものを左に、高いものを右に分ける
+            if(data[j].compareTo((T)x) <= 0){ //data[j]はx以下
                 i++;
+                //i番目とj番目を交換
+                //i番目までがx以下になる
                 Comparable<T> passingplace = data[i];
                 data[i] = data[j];
                 data[j] = passingplace;
             }
         }
+        //i+1番目とhighを交換
         Comparable<T> passingplace = data[i+1];
         data[i+1] = data[high];
         data[high] = passingplace;
