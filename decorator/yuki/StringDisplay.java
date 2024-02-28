d31f00fbdec6fa45cf4de7385cc01f67d3d6aa04
koplec <koplec>
2024-02-28 21:47:20 +0900

    日本語の文字列でもDecorateが崩れないように全角と半角での文字の長さの数え方を変えた

--- decorator/yuki/StringDisplay.java
+++ decorator/yuki/StringDisplay.java
@@ -7,9 +7,21 @@ public class StringDisplay extends Display{
     @Override
     public int getColumns() {
         // return string.getBytes().length;
-        return string.length();
+        // return string.length();
         //全角の場合は2倍必要
-        
+        int col = 0;
+        for(char ch : string.toCharArray()){
+            if (Character.UnicodeBlock.of(ch) == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS //漢字
+            || Character.UnicodeBlock.of(ch) == Character.UnicodeBlock.HIRAGANA //ひらがな
+            || Character.UnicodeBlock.of(ch) == Character.UnicodeBlock.KATAKANA //カタカナ
+            || Character.UnicodeBlock.of(ch) == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION //全角の句読点
+            ){
+                col += 2;
+            }else{
+                col += 1;
+            }
+        }
+        return col;
     }
 
     @Override
